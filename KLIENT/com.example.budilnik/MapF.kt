1890a0d77d15f02f8e4be6f4633fdd9153b5bdb9
AVAKINSHIN <avakinshin485@gmail.com>
2026-05-12 20:53:46 +0300

    Create MapF.kt

---
+++ KLIENT/com.example.budilnik/MapF.kt
@@ -0,0 +1,7 @@
+package com.example.budilnik
+class MapF(val k: String, val v: String)
+{
+    fun getValue1(): String { return k }
+    fun getValue2(): Int { return v.toInt() }
+    fun doWork() { }
+}
